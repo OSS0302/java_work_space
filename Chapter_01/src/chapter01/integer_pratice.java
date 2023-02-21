a8596bad58e9a7cd24d6a272a32aaa83358bb66c
ohsangsoo <tkdtn3264@gmail.com>
2023-02-21 16:46:38 +0900

    정수 타입 실습하기

---
+++ Chapter_01/src/chapter01/integer_pratice.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class integer_pratice {
+
+	public static void main(String[] args) {
+		int fifteen1 = 15;                     //10진수
+		byte fifteen2= 0b1111;          //2진수 15
+		short fifteen3= 017;             //8진수 15 
+		int fifteen4= 0xF;               //16진수 15
+		long lightspeed = 300000L ; // L은 long 타입임을 명시  
+	}
+
+}
