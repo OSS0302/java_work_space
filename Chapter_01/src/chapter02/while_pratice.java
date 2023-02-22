694af3b30a26737ea50f8b040636ff46c205d8ac
ohsangsoo <tkdtn3264@gmail.com>
2023-02-22 18:44:36 +0900

    while문 연습하기

---
+++ Chapter_01/src/chapter02/while_pratice.java
@@ -0,0 +1,12 @@
+package chapter02;
+
+public class while_pratice {
+	public static void main(String[] args) {
+		int i = 1;   // 초기식 
+		while (i<5) { // 조건문 
+			System.out.println(i);
+			i++; // 증감식 
+		}
+	}
+
+}
