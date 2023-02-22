b6f4ab373371a938cafe8068a103513563b71e32
ohsangsoo <tkdtn3264@gmail.com>
2023-02-22 17:07:52 +0900

    var 실습하

--- Chapter_01/src/chapter01/var_pratice.java
+++ Chapter_01/src/chapter01/var_pratice.java
@@ -1,10 +1,16 @@
 package chapter01;
 
 public class var_pratice {
-
+	//var a =1;  //var 은 메서드 내부에서만 사용할 수있다.
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int  var =1;					// var을 변수 이름으로 사용할 수있다.
+		//var x =1 ,y=3 ,z=4;  //var를 사용하여 다수의 변수를 하나의 실행문으로 초기화 할 수없다.
+		//var  str = null;			   //var변수에 null를 대입할 수없다.
+		//var  oops;                  // 변수를 바로 초기화 하지않으면 안된다.
+		//oops =1;
+		//void test (var x)       //12행 처럼 변수x 타입을 추론 할 수없다.
+		
+		
 	}
 
 }
