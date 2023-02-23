c307024d2d6aff89692995a3267185adef7dfb51
ohsangsoo <tkdtn3264@gmail.com>
2023-02-23 14:26:36 +0900

    조건문 실습하기

---
+++ Chapter_01/src/chapter02/if_pratice2.java
@@ -0,0 +1,22 @@
+package chapter02;
+
+import java.util.Scanner;
+
+public class if_pratice2 {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("숫자를 입력해주세용~!");
+		int number = in.nextInt();				// 키보트로 입력받는다.
+		
+		if (number%2==0) {
+			System.out.println("짝수 ");	//입력값이짝수 일때만 실행한다.
+		}
+		if (number %2 ==1) {
+			System.out.println("홀수 "); //입력 값이 홀 수일때만 실행한다.
+		}
+		System.out.println("종료 ");
+			
+	}
+
+}
