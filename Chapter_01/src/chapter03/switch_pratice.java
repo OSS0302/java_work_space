f594b61e5fcc6e4985587a5cf49909ace3cdc882
ohsangsoo <tkdtn3264@gmail.com>
2023-02-23 14:58:19 +0900

    스위치 구문 실습하기

---
+++ Chapter_01/src/chapter03/switch_pratice.java
@@ -0,0 +1,18 @@
+package chapter03;
+
+public class switch_pratice {
+
+	public static void main(String[] args) {
+		  int number =3 ;
+		  
+		  switch (number ){
+			  case 3:
+			  System.out.println("*");
+			  case 2:
+				System.out.println("*");  // number가 3이므로 
+			  case 1:
+				  System.out.println("*"); //break 문이 없으므
+		  }
+	}
+
+}
