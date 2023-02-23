3ac8c3da25efdace02097cc5e99e8c08cdf8db67
ohsangsoo <tkdtn3264@gmail.com>
2023-02-23 13:47:00 +0900

    조건 연산자 습하기

---
+++ Chapter_01/src/chapter01/ture_false.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+public class ture_false {
+
+	public static void main(String[] args) {
+		int x =1;
+		int y;
+		y =(x==1 )? 10:20;
+		System.out.println(y);
+		y = (x>1)? x++ :x+20;
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
