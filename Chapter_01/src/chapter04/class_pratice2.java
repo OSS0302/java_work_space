8149643f84c1ca9e41b207ec281c57f73fde8c09
ohsangsoo <tkdtn3264@gmail.com>
2023-02-25 18:02:33 +0900

    지역변수 필드변수 선언 하는방법

---
+++ Chapter_01/src/chapter04/class_pratice2.java
@@ -0,0 +1,19 @@
+package chapter04;
+
+public class class_pratice2 {
+
+	public static void main(String[] args) {
+		int a =0;
+		double b;
+		
+		//System.out.println(b); 지역변수는 초기화 되지않고는 사용할 수없다.
+		//System.out.println(b); 지역변수는c 변수는 아직선어되지 않아서 사용할 수없다.
+		int c =0;
+		// public double d= 0.0;
+		for (int e =0; e<10; e++) {
+			//int a =1; 지역변수의 이름이 다시는 선언할  수 없다. 
+			System.out.println(e);
+			}	
+	}
+
+}
