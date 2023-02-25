3395cd6175bc144fe7dde627469881da41190e2f
ohsangsoo <tkdtn3264@gmail.com>
2023-02-25 18:25:41 +0900

    클래스 의 멤버 접근하기 실

---
+++ Chapter_01/src/chapter04/Circle.java
@@ -0,0 +1,16 @@
+package chapter04;
+
+public class Circle {
+	
+	double radius;    //초기화 하지 않으므로 기본값이 0.0이여야한다. 
+	
+	double findArea() {
+		return 3.14 * radius * radius;  //클래스의 멤버에 접근한다. radius 대신에  this.radius 를사용해도 된다.
+	}
+	void show(double x , double y) {
+		System.out.printf("반지름 = %.1f, 넓이 =%1f\n" ,x ,y);
+	
+	}
+	
+
+}
