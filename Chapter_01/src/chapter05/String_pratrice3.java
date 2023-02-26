9f2ba01afeb695ad296c9eb732d2999f0fb2d670
ohsangsoo <tkdtn3264@gmail.com>
2023-02-26 12:23:09 +0900

    클래스의 정적 메서드 이용 삭제되서 다시작성

--- Chapter_01/src/chapter05/String_pratrice3.java
+++ Chapter_01/src/chapter05/String_pratrice3.java
@@ -2,4 +2,11 @@ package chapter05;
 
 public class String_pratrice3 {
 
+	public static void main(String[] args) {
+	 String fruits = String.join(",","apple ","banana","cherry","durian");
+	 System.out.println(fruits);
+	 String pi =String.valueOf(3.14);
+	 System.out.println(pi);
+	 
+	}
 }
