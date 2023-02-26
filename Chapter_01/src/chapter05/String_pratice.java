c3625ee31b18cf7a68cfa3a62255c21183374d22
ohsangsoo <tkdtn3264@gmail.com>
2023-02-26 12:15:50 +0900

    문자열 과 정수 가 결합 되면 최종적으로 문자열이 된다.

---
+++ Chapter_01/src/chapter05/String_pratice.java
@@ -0,0 +1,12 @@
+package chapter05;
+
+public class String_pratice {
+
+	public static void main(String[] args) {
+		int  i  = 7;
+		System.out.println("Java"+i);
+		System.out.println("java"+7);
+		System.out.println(7+1+"java"+7+1);  // 문자열 과 정수 가 결합 되면 최종적으로 문자열이 된다.
+	}
+
+}
