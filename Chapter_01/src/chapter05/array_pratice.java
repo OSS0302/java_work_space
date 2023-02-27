35c93b2f40beda1763a3f87976cedc1c794ccf3a
ohsangsoo <tkdtn3264@gmail.com>
2023-02-27 18:17:04 +0900

    배열 개념 주석문 추가하기

--- Chapter_01/src/chapter05/array_pratice.java
+++ Chapter_01/src/chapter05/array_pratice.java
@@ -6,17 +6,18 @@ public class array_pratice {
 
 	public static void main(String[] args) {
 	Scanner scanner =new Scanner(System.in);
-	int scores[] =new int [5];
+	int scores[] =new int [5]; // 배열을 선언하고 동시에 5개원소를 가진 배열 객체를 생성한다.
 	int sum =0;
-	for (int i =0; i <scores.length; i++) 
-	{
-		scores[i] = scanner.nextInt();
-		
-	for (int j =0; j<scores.length;j++) 
-		{
-		sum +=scores[i];
-System.out.println("평균 ="+sum /5.0);
-		}
+	for (int i =0; i <scores.length; i++) {
+	
+		scores[i] = scanner.nextInt();  //키보드에서 입력받은 정수를 배열에 대입한다.
 	}
-}  //main	가
+	for (int i =0; i<scores.length;i ++) { //scores.length  scores 배열의 크기를 나타낸다.
+		
+		sum +=scores[i];   //배열에 저장된정숫값을 모두 더한다. 
+	}
+System.out.println("평균 ="+sum /5.0);
+		
+	
+	}  //main	가
 }
