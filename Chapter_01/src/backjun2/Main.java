4ac58d11ac9a0312bde671f5208523a4615ad7a8
ohsangsoo <tkdtn3264@gmail.com>
2023-02-28 19:28:21 +0900

    1001번 문제풀이

--- Chapter_01/src/backjun2/Main.java
+++ Chapter_01/src/backjun2/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		Scanner scanner =new Scanner(System.in);
 		int num1 =scanner.nextInt();  //num1 에대한 입력함수
 		int num2 =scanner.nextInt(); //num2에 대한입력함수 
-		System.out.println(num1-num2);
+		System.out.println(num1-num2); //뺴
 		scanner.close(); //스캔함수 를 닫아야한다.
 	}
 
