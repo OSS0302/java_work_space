b59df9abfd4faa2a409c71aa4d8bae2ab5c1a871
ohsangsoo <tkdtn3264@gmail.com>
2023-03-04 07:45:12 +0900

    18108 백준 문제 풀이

---
+++ Chapter_01/src/backjun7/Main.java
@@ -0,0 +1,14 @@
+package backjun7;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner =new Scanner(System.in);
+		int years =scanner.nextInt();  //num1 에대한 입력함수
+		System.out.println(years-543);
+		
+		scanner.close();
+	}
+
+}
