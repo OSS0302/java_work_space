93480d480f0e27571b3e618e248121fd0408ed61
ohsangsoo <tkdtn3264@gmail.com>
2023-03-03 20:49:31 +0900

    1008번 나누기

---
+++ Chapter_01/src/backjun4/Main.java
@@ -0,0 +1,14 @@
+package backjun4;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner =new Scanner(System.in);
+		int num1 =scanner.nextInt();  //num1 에대한 입력함수
+		int num2 =scanner.nextInt(); //num2에 대한입력함수 
+		System.out.println(num1*num2); //곱하기 
+		scanner.close(); //스캔함수 를 닫아야한다.
+	}
+
+}
