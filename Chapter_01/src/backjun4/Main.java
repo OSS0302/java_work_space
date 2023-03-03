8891adede462d19f25306fc785dca457c58a291a
ohsangsoo <tkdtn3264@gmail.com>
2023-03-03 20:51:26 +0900

    1008번 문제 풀이

--- Chapter_01/src/backjun4/Main.java
+++ Chapter_01/src/backjun4/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner =new Scanner(System.in);
-		int num1 =scanner.nextInt();  //num1 에대한 입력함수
-		int num2 =scanner.nextInt(); //num2에 대한입력함수 
-		System.out.println(num1*num2); //곱하기 
+		double num1 =scanner.nextInt();  //num1 에대한 입력함수
+		double  num2 =scanner.nextInt(); //num2에 대한입력함수 
+		System.out.println(num1*num2); // 나누기 
 		scanner.close(); //스캔함수 를 닫아야한다.
 	}
 
